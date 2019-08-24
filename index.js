5d40c14a8a9597b3cbb36d69d33ba0f9959f597f
joaoh <joao_camargo10@hotmail.com>
2019-08-24 10:09:01 -0300

    Criando um drawer

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import { registerRootComponent } from 'expo';
 import { activateKeepAwake } from 'expo-keep-awake';
 
 import App from './src/App';
+import Menu from './src/Menu'
 
 if (__DEV__) {
   activateKeepAwake();
 }
 
-registerRootComponent(App); 
+registerRootComponent(Menu); 
---
+++ src/Menu.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+import { createDrawerNavigator } from 'react-navigation'
+
+import Simples from './componentes/Simples'
+import ParImpar from './componentes/ParImpar'
+import {Inverter, MegaSena} from './componentes/Multi'
+
+export default createDrawerNavigator({
+    MegaSena: {
+        screen: () => <MegaSena numeros={8} />,
+        navigationOptions: {title: 'Mega Sena'}
+    },
+    Inverter: {
+        screen: () => <Inverter texto="React Native" />,
+    },
+    ParImpar: {
+        screen: () => <ParImpar numero={30} />,
+        navigationOptions: {title: 'Par e Impar'}
+    },
+    Simples: {
+        screen: () => <Simples texto="Teste Teste" />,
+    },
+}, {drawerWidth: 300})
